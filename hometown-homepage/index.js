7f79d21875270bc3b0e3934c3c4fb2ed344fc720
ediallo <elhhabdouldiallo@gmail.com>
2024-09-05 13:35:31 +0100

    adds dark and light modes features

--- hometown-homepage/index.js
+++ hometown-homepage/index.js
@@ -1,15 +1,34 @@
-function convertToNumber(value) {
-  // Convert a string value to a number if possible
-  let number_value = Number(value);
-  if (Number.isNaN(number_value)) {
-    return 0;
-  } else {
-    return number_value;
-  }
-}
+const body = document.body;
+const darkModeBtn = document.querySelector("#dark_mode");
+// const lightModeBtn = document.querySelector("#dark_mode");
+console.log("darkMode ", darkModeBtn);
 
-document.getElementById("button").addEventListener("click", (event) => {
-  window.speechSynthesis.speak(
-    new SpeechSynthesisUtterance(document.getElementById("text").value)
-  );
+darkModeBtn.addEventListener("click", (event) => {
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+  document.querySelector("#about-me").style.backgroundColor = "black";
 });
+
+const lightModeBtn = document.createElement("button");
+const heroSection = document.querySelector(".hero");
+const titleSection = document.querySelector(".title");
+lightModeBtn.setAttribute("id", "light_mode");
+lightModeBtn.innerHTML = `<span class="material-symbols-outlined">
+light_mode
+</span>`;
+// get hero section
+heroSection.insertBefore(lightModeBtn, titleSection);
+
+lightModeBtn.addEventListener("click", () => {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+  document.querySelector("#about-me").style.backgroundColor = "white";
+});
+
+// create a div to wrap light mode and dark mode button
+const wrapLightDarkModes = document.createElement("div");
+wrapLightDarkModes.classList.add("modes_wrapper");
+console.log("wrapLightDarkModes ", wrapLightDarkModes);
+wrapLightDarkModes.append(lightModeBtn);
+wrapLightDarkModes.appendChild(darkModeBtn);
+heroSection.insertBefore(wrapLightDarkModes, titleSection);
