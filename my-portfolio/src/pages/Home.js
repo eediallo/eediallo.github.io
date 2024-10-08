58280237bba22c854d3b31a4f0f52da763b6f548
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 09:23:32 +0100

    updates home page

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <h1>This is the Home page</h1>;
+  return (
+    <>
+      <main className="main-section">
+        <h1>Elhadj Abdoul Diallo 👨🏻‍💻 </h1>
+        <p>Software Engineer | Speaker | Volunteer</p>
+        <p>
+          Thank you for swinging by. In this website you will find all the
+          projects I have been working on since I joined{" "}
+          <a href="https://codeyourfuture.io/">Code Your Future</a>
+        </p>
+        <p><Link className="checkout-my-work-btn" to="/portfolio">Checkout my work</Link></p>
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
