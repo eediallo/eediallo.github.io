ec1d1726190765098f3a0aeaaf670761c767f832
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 10:15:49 +0100

    adds personal site project route

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,6 +5,7 @@ import PortfolioPage from "./pages/Portfolio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import IntroToDigital from "./pages/IntroToDigital";
+import PersonalSiteProject from "./projects/ITD/personal-site/PersonalSite";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
           <Route path="/intro-to-digital" element={<IntroToDigital />} />
+          <Route path="/personal-site" element={<PersonalSiteProject />} />
         </Routes>
       </BrowserRouter>
     </div>
