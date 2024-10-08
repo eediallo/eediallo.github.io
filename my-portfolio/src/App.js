c502a12b6f2f49787d6d1c15345ff0a498536a49
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 08:48:53 +0100

    updates app.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AboutPage from "./pages/About";
+import HomePage from "./pages/Home";
+import PortfolioPage from "./pages/Portfolio";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
