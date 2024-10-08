baafe17d1fe879fb3d95b0ab789435cb556b0ada
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 09:34:11 +0100

    adds to route to itd

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/Home";
 import PortfolioPage from "./pages/Portfolio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import IntroToDigital from "./pages/IntroToDigital";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/intro-to-digital" element={<IntroToDigital />} />
         </Routes>
       </BrowserRouter>
     </div>
