92f3560dfb85aedd735e94edd127ed6c5765ee9a
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 09:31:18 +0100

    adds links to itd and itp pages

--- my-portfolio/src/pages/Portfolio.js
+++ my-portfolio/src/pages/Portfolio.js
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
 const PortfolioPage = () => {
-    return <h1>This is the Portfolio page</h1>;
-  };
-  
-  
-  export default PortfolioPage
+  return (
+    <div className="portfolio-section">
+      <h1>This is the portfolio page</h1>
+      <ul>
+        <li>
+          <Link to="/intro-to-digital">Intro to Digital</Link>
+        </li>
+        <li>
+          <Link to="/intro-to-programming">Intro to Programming</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default PortfolioPage;
