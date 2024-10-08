f84ff1cb15022bb9832a85c02f00323f734d2817
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 10:31:48 +0100

    adds link to google clone projec

--- my-portfolio/src/pages/IntroToDigital.js
+++ my-portfolio/src/pages/IntroToDigital.js
@@ -6,7 +6,12 @@ const IntroToDigital = () => {
       <h1>This is the ITD page</h1>
       <p>Welcome</p>
       <ul>
-        <li><Link to="/personal-site">Personal site</Link></li>
+        <li>
+          <Link to="/personal-site">Personal Site</Link>
+        </li>
+        <li>
+          <Link to="/google-clone">Google Clone</Link>
+        </li>
       </ul>
     </>
   );
