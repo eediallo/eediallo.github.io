49475624e319c728f4b020147e4cb68f12abf406
ediallo <elhhabdouldiallo@gmail.com>
2024-10-08 10:16:06 +0100

    link personal site project route

--- my-portfolio/src/pages/IntroToDigital.js
+++ my-portfolio/src/pages/IntroToDigital.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const IntroToDigital = () => {
   return (
     <>
       <h1>This is the ITD page</h1>
       <p>Welcome</p>
+      <ul>
+        <li><Link to="/personal-site">Personal site</Link></li>
+      </ul>
     </>
   );
 };
