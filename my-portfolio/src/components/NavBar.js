92b3b1c00a37b4cf8add792445c185dcea228c61
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 00:05:19 +0100

    adds skills and contact to nav bar

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -17,7 +17,13 @@ const NavBar = () => {
             <Link to="/about">About</Link>
           </li>
           <li>
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li>
             <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
