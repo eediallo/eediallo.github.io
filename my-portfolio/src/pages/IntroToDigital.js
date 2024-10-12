057958a2cb977f465cf6eddb3bea2d932ee6fc35
eediallo <elhhabdouldiallo@gmail.com>
2024-10-12 14:15:51 +0100

    changes ul tag to div

--- my-portfolio/src/pages/IntroToDigital.js
+++ my-portfolio/src/pages/IntroToDigital.js
@@ -4,27 +4,64 @@ const IntroToDigital = () => {
   return (
     <div className="intro-to-digital">
       <h1>Intro to Digital Projects</h1>
-      <ul className="itd-grid-list">
-        <li>
-          <Link to="https://elhadjdiallo.netlify.app/personal-site/" target="blank">Personal Site</Link>
-        </li>
-        <li>
-          <Link to="https://elhadjdiallo.netlify.app/google-com/" target="blank">Google Clone</Link>
-        </li>
-        <li>
-          <a
-            href="https://elhadji-digital-business-card.netlify.app/"
+      <div className="itd-grid-list">
+        <div className="itd-grid-item">
+          <h1>Personal Site</h1>
+          <Link
+            to="https://elhadjdiallo.netlify.app/personal-site/"
+            target="blank "
+          >
+            Learn More
+          </Link>
+        </div>
+
+        <div className="itd-grid-item">
+          <h1>Google Clone</h1>
+          <Link
+            to="https://elhadjdiallo.netlify.app/google-com/"
             target="blank"
           >
-            Digital Business Card
-          </a>
-        </li>
+            Learn More
+          </Link>
+        </div>
 
-        <li><a href="https://elhadj-birthday-gift.netlify.app/" target="blank">Birthday Gift</a></li>
+        <div className="itd-grid-item">
+          <h1> Digital Business Card</h1>
+          <Link
+            to="https://elhadji-digital-business-card.netlify.app/"
+            target="blank"
+          >
+            Learn More
+          </Link>
+        </div>
 
-        <li><Link to="https://elhadjdiallo.netlify.app/space-exploration/" target="blank">Space Exploration</Link></li>
-        <li><Link to="https://elhadjdiallo.netlify.app/hometown-homepage/" target="blank">Home Town Home Page</Link></li>
-      </ul>
+        <div className="itd-grid-item">
+          <h1> Birthday Gift</h1>
+          <Link to="https://elhadj-birthday-gift.netlify.app/" target="blank">
+            Learn More
+          </Link>
+        </div>
+
+        <div className="itd-grid-item">
+          <h1> Space Exploration</h1>
+          <Link
+            to="https://elhadjdiallo.netlify.app/space-exploration/"
+            target="blank"
+          >
+            Learn More
+          </Link>
+        </div>
+
+        <div className="itd-grid-item">
+          <h1> Home Town Home Page</h1>
+          <Link
+            to="https://elhadjdiallo.netlify.app/hometown-homepage/"
+            target="blank"
+          >
+            Learn More
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
