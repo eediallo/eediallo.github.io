8978b11cc7c101450d5413de00e6c20db53a6a93
eediallo <elhhabdouldiallo@gmail.com>
2024-10-12 08:53:05 +0100

    deletes projects folder

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,10 +5,6 @@ import PortfolioPage from "./pages/Portfolio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import IntroToDigital from "./pages/IntroToDigital";
-import GoogleClone from "./projects/ITD/Google.com/GoogleClone";
-import PersonalSite from "./projects/ITD/personal-site/PersonalSite";
-import SpaceExploration from "./projects/ITD/space-exploration/SpaceExploration";
-import HomeTownHomePage from "./projects/ITD/hometown-homepage/HomeTownHomePage";
 import IntroToProgramming from "./components/ITP";
 
 function App() {
@@ -22,10 +18,6 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
           <Route path="/intro-to-digital" element={<IntroToDigital />} />
-          <Route path="/personal-site" element={<PersonalSite />} />
-          <Route path="/google-clone" element={<GoogleClone />} />
-          <Route path="/space-exploration" element={<SpaceExploration />} />
-          <Route path="/home-town-home-page" element={<HomeTownHomePage />} />
           <Route path="/intro-to-programming" element={<IntroToProgramming />} />
         </Routes>
         </div>
