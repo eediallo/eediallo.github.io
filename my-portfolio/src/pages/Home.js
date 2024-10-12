66b14e1a715726f1287ac48b87ca7f9c32c61ef6
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 00:53:14 +0100

    changes main section layout

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
+import ProfilePicture from "../images/profile-picture.jpg";
 
 const HomePage = () => {
   return (
     <>
-      <main className="home-main">
-        <h1>Elhadj Abdoul Diallo 👨🏻‍💻 </h1>
-        <p>Software Engineer | Speaker | Volunteer</p>
+      <main className="main-home">
+        <div><img id="profile-picture" src={ProfilePicture} alt="Profile picture of Elhadj Abdoul Diallo"></img></div>
+        <div>
+      <p id="title">Software Engineer | Speaker | Volunteer</p>
+        <h1 id="name">Elhadj Abdoul Diallo 👨🏻‍💻 </h1>
         <p>
           Thank you for swinging by. In this website you will find all the
           projects I have been working on since I joined{" "}
           <a href="https://codeyourfuture.io/">Code Your Future</a>
         </p>
         <p><Link className="checkout-my-work-btn" to="/portfolio">Checkout my work</Link></p>
+        </div>
       </main>
     </>
   );
