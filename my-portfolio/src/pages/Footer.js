5bf49bdb98089aae5daae8ef666467355e2e323f
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 01:22:55 +0100

    adds footer

---
+++ my-portfolio/src/pages/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://www.linkedin.com/in/elhadj-diallo/" target="blank">
+          LinkedIn
+        </a>
+        <a href="github.com/eediallo" target="blank">
+          {" "}
+          GitHub
+        </a>
+      </div>
+
+      <div>
+        <p>© 2021 My Portfolio</p>
+      </div>
+    </footer>
+  );
+};
+
+
+export default Footer;
