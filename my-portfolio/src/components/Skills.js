0276358453ec054b693460e27136bd702c1178a7
eediallo <elhhabdouldiallo@gmail.com>
2024-10-14 16:52:49 +0100

    creates Skills component

---
+++ my-portfolio/src/components/Skills.js
@@ -0,0 +1,15 @@
+const Skills = () => {
+  return (
+    <>
+      <div>
+        <h1>Front-end Skills</h1>
+      </div>
+
+      <div>
+        <h1>Back-end Skills</h1>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
