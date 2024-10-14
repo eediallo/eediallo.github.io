f58e48eb610e7e50a21d010a8764bf43f07f8a14
eediallo <elhhabdouldiallo@gmail.com>
2024-10-14 12:15:40 +0100

    updates nav bar

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -4,33 +4,25 @@ import HamburgerIcon from "../images/hamburger.png";
 import CloseIcon from "../images/close.png";
 
 const NavBar = () => {
-
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = ()=>{
+  const toggleMenu = () => {
     setMenuOpen(!menuOpen);
-  }
-
+  };
 
   return (
     <header>
       <nav className="nav-bar">
         <ul className="nav-left">
           <li>
-            <Link to="/"><span className="e">𝖊</span>DIALLO</Link>
+            <Link to="/">
+              <span className="e">𝖊</span>DIALLO
+            </Link>
           </li>
         </ul>
 
-        {/* Hamburger icon */}
-        {/* Hamburger icon */}
-        <div className="hamburger" onClick={toggleMenu}>
-          <div>
-            {menuOpen ? <img src={CloseIcon} alt="close" /> : <img src={HamburgerIcon} alt="hamburger" />}
-            </div>
-        </div>
-
-      
-        <ul className={`nav-right ${menuOpen ? 'open' : ''}`}>
+        {/* Navigation bar for large screens */}
+        <ul className="nav-right">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -47,6 +39,48 @@ const NavBar = () => {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
+
+        {/* Hamburger icon */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <div>
+            {menuOpen ? (
+              <img src={CloseIcon} alt="close" />
+            ) : (
+              <img src={HamburgerIcon} alt="hamburger" />
+            )}
+          </div>
+        </div>
+
+        {/* Full-screen menu */}
+        {menuOpen && (
+          <div className="menu-small">
+            <div className="menu-ul-small-overlay">
+              <div className="menu-ul-small-overlay-logo">
+                <Link to="/" onClick={toggleMenu}>
+                  <span className="e">𝖊</span>DIALLO
+                </Link>
+              </div>
+
+              <div className="menu-ul-small-overlay-box">
+                <Link to="/" onClick={toggleMenu}>
+                  Home
+                </Link>
+                <Link to="/about" onClick={toggleMenu}>
+                  About
+                </Link>
+                <Link to="/skills" onClick={toggleMenu}>
+                  Skills
+                </Link>
+                <Link to="/portfolio" onClick={toggleMenu}>
+                  Portfolio
+                </Link>
+                <Link to="/contact" onClick={toggleMenu}>
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
