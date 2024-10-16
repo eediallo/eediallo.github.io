37e26423f7038de0592146bcc2ae2a3ca96f069f
eediallo <elhhabdouldiallo@gmail.com>
2024-10-16 17:15:19 +0100

    removes itd page and replaces it with portfolio page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,12 +4,12 @@ import "./css/Footer.css"
 import "./css/skills.css"
 import "./css/contact.css"
 import "./css/contact.css"
+import "./css/portfolio.css"
 import AboutPage from "./pages/About";
 import HomePage from "./pages/Home";
-import PortfolioPage from "./pages/Portfolio";
+import Portfolio from "./pages/Portfolio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import IntroToDigital from "./pages/IntroToDigital";
 import IntroToProgramming from "./components/ITP";
 import Footer from "./pages/Footer";
 import Skills from "./components/Skills";
@@ -24,8 +24,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/portfolio" element={<PortfolioPage />} />
-          <Route path="/intro-to-digital" element={<IntroToDigital />} />
+          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/intro-to-programming" element={<IntroToProgramming />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
--- my-portfolio/src/pages/Portfolio.js
+++ my-portfolio/src/pages/Portfolio.js
@@ -1,19 +1,82 @@
-import { Link } from "react-router-dom";
 
-const PortfolioPage = () => {
+const Portfolio = () => {
   return (
     <div className="portfolio-section">
       <h1>Portfolio</h1>
-      <ul className="portfolio-grid-list">
-        <li>
-          <Link to="/intro-to-digital">Intro to Digital</Link>
-        </li>
-        <li>
-          <Link to="/intro-to-programming">Intro to Programming</Link>
-        </li>
-      </ul>
+      <div className="portfolio-grid-list">
+        <div className="portfolio-grid-item">
+          <h2>Personal Site</h2>
+          <div className="a-wrap">
+            <a
+              href="https://elhadjdiallo.netlify.app/personal-site/"
+              target="blank "
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+
+        <div className="portfolio-grid-item">
+          <h2>Google Clone</h2>
+
+          <div className="a-wrap">
+            <a
+              href="https://elhadjdiallo.netlify.app/google-com/"
+              target="blank"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+
+        <div className="portfolio-grid-item">
+          <h2> Digital Business Card</h2>
+
+          <div className="a-wrap">
+            <a
+              href="https://elhadji-digital-business-card.netlify.app/"
+              target="blank"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+
+        <div className="portfolio-grid-item">
+          <h2> Birthday Gift</h2>
+          <div className="a-wrap">
+            <a href="https://elhadj-birthday-gift.netlify.app/" target="blank">
+              Learn More
+            </a>
+          </div>
+        </div>
+
+        <div className="portfolio-grid-item">
+          <h2> Space Exploration</h2>
+          <div className="a-wrap">
+            <a
+              href="https://elhadjdiallo.netlify.app/space-exploration/"
+              target="blank"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+
+        <div className="portfolio-grid-item">
+          <h2> Home Town Home Page</h2>
+          <div className="a-wrap">
+            <a
+              href="https://elhadjdiallo.netlify.app/homehrefwn-homepage/"
+              target="blank"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PortfolioPage;
+export default Portfolio;
