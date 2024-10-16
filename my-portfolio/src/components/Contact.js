63f0ef60298170a8be8475d50ce90e0bfc4451c0
eediallo <elhhabdouldiallo@gmail.com>
2024-10-16 11:07:46 +0100

    adds contact information

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,12 +1,23 @@
 import phoneLogo from "../images/phone-logo.svg"
+import emailLogo from "../images/email-logo.svg"
+import locationLogo from "../images/location.svg"
+
 
 const Contact = ()=>{
     return(
         <div className="contact-wrapper">
             <h1>Contact</h1>
-            <div>
+            <div className="contact-box">
                 <img src={phoneLogo} alt="phone logo"/>
                 <h2>+44 8876557867</h2>
+            </div>
+            <div className="contact-box">
+                <img src={emailLogo} alt="phone logo"/>
+                <h2>elhhabdouldiallo@gmail.com</h2>
+            </div>
+            <div className="contact-box">
+                <img src={locationLogo} alt="phone logo"/>
+                <h2>London, UK</h2>
             </div>
         </div>
     )
