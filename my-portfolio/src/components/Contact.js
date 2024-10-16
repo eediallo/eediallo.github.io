f29a0bba8cc904642296405a87ced6a86fe2fd26
eediallo <elhhabdouldiallo@gmail.com>
2024-10-16 12:56:01 +0100

    adds and styles contact page

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,26 +1,63 @@
-import phoneLogo from "../images/phone-logo.svg"
-import emailLogo from "../images/email-logo.svg"
-import locationLogo from "../images/location.svg"
+import phoneLogo from "../images/phone-logo.svg";
+import emailLogo from "../images/email-logo.svg";
+import locationLogo from "../images/location.svg";
 
-
-const Contact = ()=>{
-    return(
-        <div className="contact-wrapper">
-            <h1>Contact</h1>
-            <div className="contact-box">
-                <img src={phoneLogo} alt="phone logo"/>
-                <h2>+44 8876557867</h2>
-            </div>
-            <div className="contact-box">
-                <img src={emailLogo} alt="phone logo"/>
-                <h2>elhhabdouldiallo@gmail.com</h2>
-            </div>
-            <div className="contact-box">
-                <img src={locationLogo} alt="phone logo"/>
-                <h2>London, UK</h2>
-            </div>
+const Contact = () => {
+  return (
+    <>
+    <h1 className="contact-title">Contact</h1>
+    <div className="contact-wrapper">
+      <div>
+        <div className="contact-box">
+          <img src={phoneLogo} alt="phone logo" />
+          <p><important>+44 9939 99393939</important></p>
         </div>
-    )
-}
+        <div className="contact-box">
+          <img src={emailLogo} alt="phone logo" />
+          <p><import>elhhabdouldiallo@gmail.com</import></p>
+        </div>
+        <div className="contact-box">
+          <img src={locationLogo} alt="phone logo" />
+          <p><important>London, UK</important></p>
+        </div>
+      </div>
 
-export default Contact
+      <div className="contact-form">
+        <form>
+            {/* main form input */}
+            <div>
+            <div>
+                <label for="full-name" >Full Name: </label>
+                <input type="text" id="full-name" name="full-name" placeholder="Enter your full name" required/>
+            </div>
+            <br></br>
+            <div>
+                <label for="email" >Email Address: </label>
+                <input type="email" id="email" name="email" placeholder="Enter your email address" required/>
+            </div>
+            <br></br>
+
+            <div>
+                <label for="enquiry" >Enquiry Type: </label>
+                <select id="enquiry" name="enquiry" >
+                    <option>EDA Academy</option>
+                    <option>Maintenance</option>
+                    <option>Partnership</option>
+                </select>
+            </div>
+            </div>
+
+            {/* text area */}
+            <div>
+                <label for="message"></label><br/>
+                <textarea id="message" name="message" rows={8} cols={50} placeholder="write your message here">
+                </textarea>
+            </div>
+        </form>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Contact;
